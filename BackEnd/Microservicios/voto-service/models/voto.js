a87fc5f975e0cf1d4291d4052a06ed225b107900
Andres101215 <andres.puentes02@uptc.edu.co>
2025-04-08 10:48:29 -0500

    voto service

--- BackEnd/Microservicios/voto-service/models/voto.js
+++ BackEnd/Microservicios/voto-service/models/voto.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const votoSchema = new mongoose.Schema({
   electionId: String,
   candidateId: String,
-  voterId: String,
+  token: String,
   timestamp: { type: Date, default: Date.now },
   status: { type: String, default: "valid" }
 });
